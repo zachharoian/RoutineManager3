402c3153f33ffad6dc2d415d3a10526433fda067
John Haroian <john@nwtechguy.com>
2017-01-08 12:41:45 -0800

    Fixes segue to Add Event

---
+++ App12/NewEventController.cs
@@ -0,0 +1,13 @@
+using Foundation;
+using System;
+using UIKit;
+
+namespace App12
+{
+    public partial class NewEventController : UITableViewController
+    {
+        public NewEventController (IntPtr handle) : base (handle)
+        {
+        }
+    }
+}
