ae74bb3a5423220e4794d6e1dbf38b1f8e49bf87
zach <zach@ZACHSPC>
2017-02-05 12:42:35 -0800

    Initial save before revisions

---
+++ App12/ContentViewController.cs
@@ -0,0 +1,13 @@
+﻿using Foundation;
+using System;
+using UIKit;
+
+namespace App12
+{
+    public partial class ContentViewController : UIViewController
+    {
+        public ContentViewController (IntPtr handle) : base (handle)
+        {
+        }
+    }
+}
---
+++ App12/ViewController.cs
@@ -0,0 +1,16 @@
+﻿using Foundation;
+using System;
+using UIKit;
+
+namespace App12
+{
+    public partial class ViewController : UIViewController
+    {
+
+        UIPageViewController pageViewController = new UIPageViewController();
+        
+        public ViewController (IntPtr handle) : base (handle)
+        {
+        }
+    }
+}
